6ae9b5611e54bf4d89309550884ab35d82644f50
Nama Anda <email@example.com>
2024-11-10 19:28:55 -0800

    Pesan untuk perubahan terbaru

--- pertemuan 12/index.js
+++ pertemuan 12/index.js
@@ -1,306 +1,61 @@
-//JavaScript Loop
+// String literals, Arrow Function &
+// Default Parameter
 
-//Array Object
+// 1. String Literals
+let fullName = "John Doe";
+let age = 30;
+let address = "Manado";
 
-let students = [
-  {
-    id: 1,
-    firstName: "John",
-    grade: 80,
-  },
-  {
-    id: 2,
-    firstName: "Jane",
-    grade: 90,
-  },
-  {
-    id: 3,
-    firstName: "Bob",
-    grade: 100,
-  },
-];
+// Halo nama saya John Doe, umur saya 33 tahun
+// dan saya tinggal di Manado
 
-console.log(students);
+let kalimat =
+  "Halo nama saya " +
+  fullName +
+  ", umur saya " +
+  age +
+  " tahun dan saya tinggal di " +
+  address;
 
-//1. for - loop
-// for (let i = 0; i < students.length; i++) {
-//   console.log(students[i].firstName);
-// }
+console.log(kalimat);
 
-//2. while - loop
-// let i = 0;
-// while (i < students.length) {
-//   console.log(students[i].firstName);
-//   i++;
-// }
+let kalimat2 = `Halo nama saya ${fullName}, umur saya ${age} tahun dan saya tinggal di ${address}`;
+console.log(kalimat2);
 
-//3. do - while - loop
-// let i = 0;
-// do {
-//   console.log(students[i].firstName);
-//   i++;
-// } while (i < students.length);
+//2. Arrow Function
 
-//4. Array built-in method
-//4.a foreach()
+function sayGreetings(fullName) {
+  return `Hello my name is ${fullName}`;
+}
 
-// students.forEach(function (item) {
-//   console.log(item.firstName);
-// });
+console.log(sayGreetings("John Doe"));
 
-//4.b map()
-// let output = students.map(function (item) {
-//   return item.firstName;
-// });
-// console.log(output);
+const sayGreetings2 = (fullName) => `Hello my name is ${fullName}`;
 
-//4.c filter()
-let output1 = students.filter(function (item) {
-  return item.grade >= 90;
-});
-console.log(output1);
+console.log(sayGreetings2("John Doe"));
 
-//4.d find()
-let output2 = students.find(function (item) {
-  return item.grade >= 90;
-});
+// Arrow function pada IIFE
+let output = (() => "Hello")();
+console.log(output);
+
+// Arrow function pada callback
+let numbers = [1, 2, 3, 4, 5];
+let output2 = numbers.map((item) => item);
 console.log(output2);
 
-//4.e findIndex()
-let output3 = students.findIndex(function (item) {
-  return item.firstName === "Jane";
-});
-console.log(output3);
+// 3. Default parameter
 
-//Mini Exercise
-let countries = [
-  "Afghanistan",
-  "Albania",
-  "Algeria",
-  "Andorra",
-  "Angola",
-  "Anguilla",
-  "Antigua &amp;Barbuda",
-  "Argentina",
-  "Armenia",
-  "Aruba",
-  "Australia",
-  "Austria",
-  "Azerbaijan",
-  "Bahamas",
-  "Bahrain",
-  "Bangladesh",
-  "Barbados",
-  "Belarus",
-  "Belgium",
-  "Belize",
-  "Benin",
-  "Bermuda",
-  "Bhutan",
-  "Bolivia",
-  "Bosnia &amp; Herzegovina",
-  "Botswana",
-  "Brazil",
-  "British Virgin Islands",
-  "Brunei",
-  "Bulgaria",
-  "Burkina Faso",
-  "Burundi",
-  "Cambodia",
-  "Cameroon",
-  "Cape Verde",
-  "Cayman Islands",
-  "Chad",
-  "Chile",
-  "China",
-  "Colombia",
-  "Congo",
-  "Cook Islands",
-  "Costa Rica",
-  "Cote D Ivoire",
-  "Croatia",
-  "Cruise Ship",
-  "Cuba",
-  "Cyprus",
-  "Czech Republic",
-  "Denmark",
-  "Djibouti",
-  "Dominica",
-  "Dominican Republic",
-  "Ecuador",
-  "Egypt",
-  "El Salvador",
-  "Equatorial Guinea",
-  "Estonia",
-  "Ethiopia",
-  "Falkland Islands",
-  "Faroe Islands",
-  "Fiji",
-  "Finland",
-  "France",
-  "French Polynesia",
-  "French West Indies",
-  "Gabon",
-  "Gambia",
-  "Georgia",
-  "Germany",
-  "Ghana",
-  "Gibraltar",
-  "Greece",
-  "Greenland",
-  "Grenada",
-  "Guam",
-  "Guatemala",
-  "Guernsey",
-  "Guinea",
-  "Guinea Bissau",
-  "Guyana",
-  "Haiti",
-  "Honduras",
-  "Hong Kong",
-  "Hungary",
-  "Iceland",
-  "India",
-  "Indonesia",
-  "Iran",
-  "Iraq",
-  "Ireland",
-  "Isle of Man",
-  "Israel",
-  "Italy",
-  "Jamaica",
-  "Japan",
-  "Jersey",
-  "Jordan",
-  "Kazakhstan",
-  "Kenya",
-  "Kuwait",
-  "Kyrgyz Republic",
-  "Laos",
-  "Latvia",
-  "Lebanon",
-  "Lesotho",
-  "Liberia",
-  "Libya",
-  "Liechtenstein",
-  "Lithuania",
-  "Luxembourg",
-  "Macau",
-  "Macedonia",
-  "Madagascar",
-  "Malawi",
-  "Malaysia",
-  "Maldives",
-  "Mali",
-  "Malta",
-  "Mauritania",
-  "Mauritius",
-  "Mexico",
-  "Moldova",
-  "Monaco",
-  "Mongolia",
-  "Montenegro",
-  "Montserrat",
-  "Morocco",
-  "Mozambique",
-  "Namibia",
-  "Nepal",
-  "Netherlands",
-  "Netherlands Antilles",
-  "New Caledonia",
-  "New Zealand",
-  "Nicaragua",
-  "Niger",
-  "Nigeria",
-  "Norway",
-  "Oman",
-  "Pakistan",
-  "Palestine",
-  "Panama",
-  "Papua New Guinea",
-  "Paraguay",
-  "Peru",
-  "Philippines",
-  "Poland",
-  "Portugal",
-  "Puerto Rico",
-  "Qatar",
-  "Reunion",
-  "Romania",
-  "Russia",
-  "Rwanda",
-  "Saint Pierre &amp; Miquelon",
-  "Samoa",
-  "San Marino",
-  "Satellite",
-  "Saudi Arabia",
-  "Senegal",
-  "Serbia",
-  "Seychelles",
-  "Sierra Leone",
-  "Singapore",
-  "Slovakia",
-  "Slovenia",
-  "South Africa",
-  "South Korea",
-  "Spain",
-  "Sri Lanka",
-  "St Kitts &amp; Nevis",
-  "St Lucia",
-  "St Vincent",
-  "St. Lucia",
-  "Sudan",
-  "Suriname",
-  "Swaziland",
-  "Sweden",
-  "Switzerland",
-  "Syria",
-  "Taiwan",
-  "Tajikistan",
-  "Tanzania",
-  "Thailand",
-  "Timor L'Este",
-  "Togo",
-  "Tonga",
-  "Trinidad &amp; Tobago",
-  "Tunisia",
-  "Turkey",
-  "Turkmenistan",
-  "Turks &amp; Caicos",
-  "Uganda",
-  "Ukraine",
-  "United Arab Emirates",
-  "United Kingdom",
-  "Uruguay",
-  "Uzbekistan",
-  "Venezuela",
-  "Vietnam",
-  "Virgin Islands (US)",
-  "Yemen",
-  "Zambia",
-  "Zimbabwe",
-];
-
-//1. Cari index dari negara Indonesia
-let index = countries.findIndex(function (item) {
-  return item === "Indonesia";
-});
-console.log(index);
-//2. Dengan menggunakan filter tampilkan semua
-//   negara yang berakhiran "sia"
-let outputCountries = countries.filter(function (item) {
-  return item.endsWith("sia");
-});
-console.log(outputCountries);
-index.js
-Displaying index.js. 
-Pertemuan 9
-Stenly Adam
-•
-Sep 8
-
-index.js
-Javascript
-Class comments
-
-Add class comment…
+const sayGreetings3 = (fullName, age, address) => {
+  if (fullName === undefined) {
+    fullName = "John Doe";
+  }
+  if (age === undefined) {
+    age = 30;
+  }
+  if (address === undefined) {
+    address = "Manado";
+  }
+  console.log(
+    `Hallo nama saya ${fullName}, umur saya ${age} tahun dan saya tinggal di ${address}`
+  );
+};
--- pertemuan 6/index.js
+++ pertemuan 6/index.js
@@ -1,6 +1,34 @@
-//Buatlah fungsi untuk menghitung BMI (Body Mass Index)
-//Rumus : BMI = berat / (tinggi * tinggi),
-//berat dalam kg dan tinggi dalam meter
-//170 cm = 1.7 m
-//Fungsi tersebut menerima 2 parameter yaitu berat dan tinggi
-//Dan mengembalikan output berupa BMI
+// IIFE & Callback Function
+// 1. IIFE (Immediately Invoked Function Expression)
+// a. No params & args & return value
+// (function () {
+//   console.log("Hello World");
+// })();
+// // b. With params & args & return value
+// let output = (function (fullName) {
+//   return "Hello " + fullName;
+// })("John Doe");
+
+// console.log(output);
+
+// 2. Callback Function
+// a. No params, args & return value
+// function sayHello(callback) {
+//   callback();
+// }
+
+// sayHello(function () {
+//   console.log("Hello World");
+// });
+
+// b. With params, args & return value
+function sayHello(callback) {
+  let result = callback("John Doe"); //Call callback func
+  return result;
+}
+
+let output = sayHello(function (fullName) {
+  return "Hello " + fullName;
+});
+
+console.log(output);
